33528cd4db857dac731292ffc98e19124414293e
rockstr99 <arpit150100@gmail.com>
2020-07-27 18:17:11 +0530

    Updates

---
+++ precache-manifest.21ef2488af7806396d25bf5db45cb40f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "414f68ae98033a6702d351c0cf778097",
+    "url": "/responsiveReactWebsite/index.html"
+  },
+  {
+    "revision": "9d1a729fa29569d8c27c",
+    "url": "/responsiveReactWebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "a28f2d8bf792f18b5096",
+    "url": "/responsiveReactWebsite/static/css/main.39d8b807.chunk.css"
+  },
+  {
+    "revision": "9d1a729fa29569d8c27c",
+    "url": "/responsiveReactWebsite/static/js/2.98be3afc.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/responsiveReactWebsite/static/js/2.98be3afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28f2d8bf792f18b5096",
+    "url": "/responsiveReactWebsite/static/js/main.680dcaed.chunk.js"
+  },
+  {
+    "revision": "91e836624a499efb0f01",
+    "url": "/responsiveReactWebsite/static/js/runtime-main.22d0532e.js"
+  },
+  {
+    "revision": "fdd9a61bf3515e248857eb684834a275",
+    "url": "/responsiveReactWebsite/static/media/card1.fdd9a61b.png"
+  },
+  {
+    "revision": "2b9cbfb62673de060a2c37acd1b511ea",
+    "url": "/responsiveReactWebsite/static/media/wfh_1.2b9cbfb6.svg"
+  },
+  {
+    "revision": "bfaa07f74c5779b9b6319459d0455c91",
+    "url": "/responsiveReactWebsite/static/media/wfh_7.bfaa07f7.svg"
+  }
+]);
